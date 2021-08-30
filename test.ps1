87e003709ae60d67edb703b398db428a2064e381
skdutta <skd@officeclip.com>
2021-08-30 17:39:26 -0400

    fixed the alt tag implementation not tested

--- test.ps1
+++ test.ps1
@@ -1,16 +1,13 @@
-# from: https://stackoverflow.com/a/1875662
-
-$fileName = "test.html"
-
-(Get-Content $fileName) | 
-    Foreach-Object {
-        $_ # send the current line to output
-        if ($_ -match "<head>") 
-        {
-            #Add Lines after the selected pattern 
-            "<!-- testing -->"
-        }
-    } | Set-Content $fileName
-
-#Add-Content -Path .\test.html -Value '<!-- Comment -->'
-#Get-Content -Path .\test.html
+Get-ChildItem -Path "C:\OfficeClipNew\opensource\Powershell" -Filter "*.html" |
+ForEach-Object {
+    $fileName = $_.FullName
+    $content = Get-Content $fileName
+     # Check if we have the alt tag with the image, then ignore it
+     $count = [regex]::matches($content,'^.*img.*alt.*$').count
+     Write-Output $Count
+    if ($count -eq 0){
+        $content1 = $content -replace '<img src="(.+/)(.+)\.(.+)"(.*)>', '<img src="$1$2.$3" alt="$2" $4>'
+        $content1 -replace 'alt=".+"', { $_.Value.replace('-', ' ').replace('_', ' ') } |
+        Set-Content $fileName
+    }
+}
