0aa85d828327eccafe2673d0465ced17dd722dcb
SK Dutta <skd@officeclip.com>
2022-01-29 16:27:42 -0500

    Saving changes made

--- test.ps1
+++ test.ps1
@@ -1,12 +1,14 @@
-Get-ChildItem -Path "C:\OfficeClipNew\opensource\Powershell" -Filter "*.html" |
+Get-ChildItem -Path "C:\OfficeClipNew\OpenSource\Powershell" -Filter "*.dita" -Recurse |
+#Get-ChildItem -Path "C:\OfficeClipNew\docs\Help\Dita\Topics\apps" -Filter "*.dita" -Recurse |
 ForEach-Object {
     $fileName = $_.FullName
     $content = Get-Content $fileName
+    Write-Output $fileName
      # Check if we have the alt tag with the image, then ignore it
-     $count = [regex]::matches($content,'^.*img.*alt.*$').count
-     Write-Output $Count
+     $count = [regex]::matches($content,'^.*image.*alt.*$').count
+    Write-Output $Count
     if ($count -eq 0){
-        $content1 = $content -replace '<img src="(.+/)(.+)\.(.+)"(.*)>', '<img src="$1$2.$3" alt="$2" $4>'
+        $content1 = $content -replace '<image.+href="(.+/)(.+)\.(.+)"(.*)', '<image href="$1$2.$3" alt="$2" $4'
         $content1 -replace 'alt=".+"', { $_.Value.replace('-', ' ').replace('_', ' ') } |
         Set-Content $fileName
     }
